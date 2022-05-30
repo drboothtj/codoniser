09a4d69ef468c20df0f016a31bb82dc0f3d9dfdd
DrBoothTJ <63703480+DrBoothTJ@users.noreply.github.com>
2022-05-30 13:39:34 +0200

    update and add example data

--- codoniser/main.py
+++ codoniser/main.py
@@ -25,6 +25,20 @@ def get_codon_table(codons):
         codon_table.append([triplet_nt[i], triplet_count[i],triplet_percentage[i]])
     return codon_table
 
+def get_codon_distribution(codons, step_size, window_size):
+    start_point = 0
+    end_point = start_point + window_size
+    while end_point < len(codons):
+        window_codons = codons [start_point:end_point]
+        mid_point = start_point + ((start_point - end_point)/2)
+        codon_count = Counter(window_codons)
+        print(codon_count)
+        #add midpoint and counts to data
+        start_point += step_size
+        end_point = start_point + window_size
+    #return x
+
+
 def main():
     io.print_to_system('Running codoniser version 0.1.0!')
     args = parser.parse_args()
@@ -42,6 +56,11 @@ def main():
     codons = get_codons(record_sequences)
     codon_table = get_codon_table(codons)
     io.list_to_csv('codon_table.csv', codon_table)
-
+    
+    step_size = 20 #parse input
+    window_size = 100 #parse input
+    get_codon_distribution(codons, step_size, window_size)
+    plot_codon_distribution
+    
 if __name__ == '__main__':
     main()
