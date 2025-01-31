52e487e1e7cb2c8347d501092ae2facea7dfd6dd
drboothtj <booth.thom@gmail.com>
2025-01-31 12:44:23 +0100

    update install reqs

--- setup.py
+++ setup.py
@@ -15,6 +15,6 @@ setup(
     url="https://github.com/DrBoothTJ/codoniser",
     license='GNU General Public License v3.0',
     python_requires='>=3.7',
-    install_requires=['bio'],
+    install_requires=['bio','matplotlib', 'seaborn', 'scipy'],
     entry_points={'console_scripts': ["codoniser=codoniser.main:main"]}
 )
